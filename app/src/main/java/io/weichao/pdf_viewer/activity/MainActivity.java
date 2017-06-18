39371da04782a4bddcb3baf57f61aba650a9aa37
weichao66666 <weichao66666@gmail.com>
2017-06-18 15:06:58 +0800

    添加 Android 6.0+ 动态获取权限。

--- app/src/main/java/io/weichao/pdf_viewer/activity/MainActivity.java
+++ app/src/main/java/io/weichao/pdf_viewer/activity/MainActivity.java
@@ -1,10 +1,14 @@
 package io.weichao.pdf_viewer.activity;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.RelativeLayout;
 
@@ -17,6 +21,10 @@ import io.weichao.pdf_viewer.R;
 import io.weichao.pdf_viewer.widget.PdfViewPager;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String[] PERMISSIONS = new String[]{
+            Manifest.permission.READ_EXTERNAL_STORAGE  // 读取权限
+    };
+
     public static void actionStart(Context context, String filePath) {
         Intent intent = new Intent(context, MainActivity.class);
         intent.setData(Uri.fromFile(new File(filePath)));
@@ -28,6 +36,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getPermissions();
+    }
+
+    private void getPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, PERMISSIONS[0]) != PackageManager.PERMISSION_GRANTED) {
+                // Android 6.0 申请权限
+                requestPermissions(PERMISSIONS, 1);
+            } else {
+                onContinueRun();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 1) {
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                finish();
+            } else {
+                onContinueRun();
+            }
+        }
+    }
+
+    private void onContinueRun() {
         RelativeLayout root = (RelativeLayout) findViewById(R.id.root);
 
         /*文件在 SD 卡中，可直接使用。*/
